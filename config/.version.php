d9d054ba1633b915c4e6c7917c5c67a0aceb4e98
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-09 16:03:15 +0300

    Файл конфигурации версии виджета

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Файл конфигурации версии виджета.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'name'        => 'Light Gallery',
+    'description' => 'Gallery widget',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'GearMagic',
+    'authorUrl'   => 'https://gearmagic.ru',
+    'email'       => 'anton.tivonenko@gmail.com',
+    'url'         => 'https://apps.gearmagic.ru/component/gm-wd-lightgallery',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://apps.gearmagic.ru/license/mit-license.html'
+];
